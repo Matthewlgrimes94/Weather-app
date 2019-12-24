669534ceebda99c467e52d7117d073841319e23c
M Grimes <matthewlgrimes@gamil.com>
2019-12-24 11:28:13 -0700

    Added localStorage support

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ var cardHolder = $('#card-holder');
 var searchForm = $('#search-form');
 submitbtn = $('#submit');
 
-var temp = 0;
-var averageTemp = 0;
-var windSpeed = 0;
-var averageWindSpeed = 0;
-var humidity = 0;
-var averageHumidity = 0;
-var weather = [];
+var temp;
+var averageTemp;
+var weather;
 var icon;
 var t;
 
@@ -19,6 +15,13 @@ if (moment().format('H') >= 20) {
   t = 'd';
 }
 
+if(localStorage.getItem('lastSearch')) {
+  var dailyForecast = (JSON.parse(localStorage.getItem('lastSearch'))).list;
+  var city = (JSON.parse(localStorage.getItem('lastSearch'))).city.name;
+  var likelyWeather = (localStorage.getItem('lastWeather'));
+  buildWeatherCards(dailyForecast, city);
+}
+
 submitbtn.click(function(e){
     e.preventDefault();
     var cityEl = $('#city-search').val();
@@ -27,11 +30,12 @@ submitbtn.click(function(e){
       url: queryURL.replace(/\s/g, "+"),
       method: 'GET',
   }).then(function(r) {
+      localStorage.setItem('lastSearch', JSON.stringify(r));
       cardHolder.empty();
       let city = r.city.name;
       let dailyForecast = r.list;
       console.log(city);
-      //console.log(r);
+      console.log(r);
       buildWeatherCards(dailyForecast,city);
   });
 })
@@ -64,29 +68,20 @@ function buildWeatherCards(dailyForecast,city) {
   for (var i = 0; i < dailyForecast.length; i += 8) {
     temp = 0;
     averageTemp = 0;
-    windSpeed = 0;
-    averageWindSpeed = 0;
-    humidity = 0;
-    averageHumidity = 0;
     weather = [];
     for(var j = 0; j < 8; j++) {
       temp += dailyForecast[(i+j)].main.temp;
-      windSpeed += dailyForecast[(i+j)].wind.speed;
-      humidity += dailyForecast[(i+j)].main.humidity;
       weather.push(dailyForecast[(i+j)].weather[0].main);
     }
     averageTemp = Math.round(temp/8);
-    averageWindSpeed = Math.round(windSpeed/8);
-    averageHumidity = Math.round(humidity/8);
     likelyWeather = mode(weather);
+    localStorage.setItem('lastWeather', `${likelyWeather}`);
     var newCard = ($(`<div class='col-sm-3 col-xs-9 m-1 ml-1 p-0'><div class="card">
     <div class="card-body">
-      <p class="card-text">${city}</p>
+      <h4 class="card-title">${city}</h4>
       <p class="card-text">${moment(dailyForecast[i].dt_txt).format('dddd')}</p>
       <p class="card-text">${moment(dailyForecast[i].dt_txt).format('L')}</p>
       <p class="card-text">${averageTemp}&#176</p>
-      <p class="card-text">Wind: ${averageWindSpeed}mph</p>
-      <p class="card-text">Humidity: ${averageHumidity}</p>
       <p class="card-text">${likelyWeather}</p>
       <img src="http://openweathermap.org/img/w/${findIcon(likelyWeather)}.png">
     </div>
